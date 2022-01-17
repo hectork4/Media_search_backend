903236cc6e96ae401ac47ad51376d189f6bc856a
hectork4 <emanuel_klikailo@yahoo.com>
2022-01-17 18:45:28 -0300

    fix mongodb issues

--- src/index.js
+++ src/index.js
@@ -1,21 +1,6 @@
 import app from './app'
 import config from './config'
-//import './database'
-import mongoose, { mongo } from "mongoose";
+import './database'
 
-const connectionString = config.MONGODB_URI
-
-const options = {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    family: 4
-}
-
-mongoose.connect(connectionString, options)
-.then(db => {
-    console.log("Db is connected")
-    app.listen(config.PORT)
-    console.log("server listen on port", 4000)  
-})
-.catch(error => console.log(error))
-
+app.listen(config.PORT)
+console.log("server listen on port", 4000)
