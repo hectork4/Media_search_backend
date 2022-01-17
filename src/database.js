341c357300ce5dafeb607ccc19a3da71d8cf0d9e
hectork4 <emanuel_klikailo@yahoo.com>
2022-01-17 16:42:21 -0300

    fix options mongodb connection

--- src/database.js
+++ src/database.js
@@ -3,9 +3,17 @@ import config from './config'
 
 const connectionString = config.MONGODB_URI
 
-mongoose.connect(connectionString, {
+const options = {
     useNewUrlParser: true,
-    useUnifiedTopology: true
-})
+    useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 5000,
+    autoIndex: false, // Don't build indexes
+    maxPoolSize: 10, // Maintain up to 10 socket connections
+    serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
+    socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
+    family: 4 // Use IPv4, skip trying IPv6
+}
+
+mongoose.connect(connectionString, options)
 .then(db => console.log("Db is connected"))
 .catch(error => console.log(error))
