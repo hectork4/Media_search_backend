71cd5f8cc740a8b0e24f4e27e5753396fb5e3dec
hectork4 <emanuel_klikailo@yahoo.com>
2022-01-17 17:39:27 -0300

    change mongodb init

--- build/index.js
+++ build/index.js
@@ -1,13 +1,33 @@
 "use strict";
 
+function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
 var _app = _interopRequireDefault(require("./app"));
 
 var _config = _interopRequireDefault(require("./config"));
 
-require("./database");
+var _mongoose = _interopRequireWildcard(require("mongoose"));
+
+function _getRequireWildcardCache(nodeInterop) { if (typeof WeakMap !== "function") return null; var cacheBabelInterop = new WeakMap(); var cacheNodeInterop = new WeakMap(); return (_getRequireWildcardCache = function _getRequireWildcardCache(nodeInterop) { return nodeInterop ? cacheNodeInterop : cacheBabelInterop; })(nodeInterop); }
+
+function _interopRequireWildcard(obj, nodeInterop) { if (!nodeInterop && obj && obj.__esModule) { return obj; } if (obj === null || _typeof(obj) !== "object" && typeof obj !== "function") { return { "default": obj }; } var cache = _getRequireWildcardCache(nodeInterop); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (key !== "default" && Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj["default"] = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
-_app["default"].listen(_config["default"].PORT);
+//import './database'
+var connectionString = _config["default"].MONGODB_URI;
+var options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  family: 4
+};
 
-console.log("server listen on port", 4000);
+_mongoose["default"].connect(connectionString, options).then(function (db) {
+  console.log("Db is connected");
+
+  _app["default"].listen(_config["default"].PORT);
+
+  console.log("server listen on port", 4000);
+})["catch"](function (error) {
+  return console.log(error);
+});
